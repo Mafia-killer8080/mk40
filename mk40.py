aaca2f036b9913039901ffece2f644d62695e876
Mafia-killer8080 <96686213+Mafia-killer8080@users.noreply.github.com>
2022-01-28 01:35:11 +0500

    Create mk40.py

---
+++ mk40.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "remove":
+			os.system("rm -f .__apikey__.txt")
+			exit(" [!] Succesfull Deleted")
+		else:
+			print(" [?] Wellcome : ")
+			exit(" [!] Run : python mk40.py remove")
+	try:
+		__import__("mk40").__main_MK()
+	except Exception as e:
+		exit(str(e))
